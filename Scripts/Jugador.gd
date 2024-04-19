5d393261f81ec25862170744f49a086408425f86
rogergualbatet <rogeretgualbatet2@gmail.com>
2024-04-19 13:43:03 +0200

    Canvis al script del personatge

--- Scripts/Jugador.gd
+++ Scripts/Jugador.gd
@@ -10,8 +10,9 @@ var velocitat_maxima = 200
 
 func _physics_process(delta):
 	# Add the gravity.
+	animated_sprite_2D.play("default")
 	if (velocity.x > 1 or velocity.x < -1) and (velocity.y > 1 or velocity.y < -1):
-		animated_sprite_2D.animation = "Running"
+		animated_sprite_2D.play("Running")
 	else:
 		animated_sprite_2D.animation = "default"
 		
@@ -20,9 +21,10 @@ func _physics_process(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("Up"):
 		#velocity += vel_salt
-		direccio.y -= 1
+		direccio.y += -1
 	if Input.is_action_pressed("Left"):
 		direccio.x += -1
+		animated_sprite_2D.animation = "Running"
 	if Input.is_action_pressed("Right"):
 		direccio.x += 1
 	#velocity += gravetat * delta
