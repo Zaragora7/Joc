8cec04f34ad961e2b30ed5353f140ceea98d5b29
rogergualbatet <rogeretgualbatet2@gmail.com>
2024-04-25 09:04:13 +0200

    Coses al personatge

--- Scripts/Jugador.gd
+++ Scripts/Jugador.gd
@@ -5,14 +5,17 @@ extends CharacterBody2D
 var vel_salt := Vector2.UP * 500
 var direccio := Vector2(0, 0)
 var velocitat_maxima = 200
+var atacar:bool = false
 @onready var animated_sprite_2D = $AnimatedSprite2D
 @onready var point_light_2d = $PointLight2D
 @onready var animated_sprite_2d_2 = $AnimatedSprite2D2
+@onready var hit_box_character = $AnimatedSprite2D2/HitBoxCharacter
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	#animated_sprite_2D.play("default")
+	
 	if (velocity.x > 1 or velocity.x < -1) or (velocity.y > 1 or velocity.y < -1):
 		animated_sprite_2D.play("Running")
 	else:
@@ -33,12 +36,18 @@ func _physics_process(delta):
 		direccio.y += 1
 	if Input.is_action_just_pressed("atac"):
 		$AnimatedSprite2D2.play("Slash")
+		atacar = true
+		
 	#velocity += gravetat * delta
+	
+		
 	velocity += direccio.normalized() * velocitat_maxima
 	move_and_slide()
 	for i in range(1, 100):
-		point_light_2d.energy -= 0.0001
+		point_light_2d.energy -= 0.00001
+		if point_light_2d.energy == 0:break
 		
 	
 	var isLeft = velocity.x < 0
 	animated_sprite_2D.flip_h = isLeft
+	animated_sprite_2d_2.flip_h = isLeft
