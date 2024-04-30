55f780efce38b9e20484a4173a01189a60dca8b0
rogergualbatet <rogeretgualbatet2@gmail.com>
2024-04-30 09:59:04 +0200

    El cavaller pot atacar
    el cavaller te una hitbox davant seu que, al activar la animacio de atac apareix, cal pulir-lo molt encara

--- escenes/Cavaller_principal.gd
+++ escenes/Cavaller_principal.gd
@@ -5,6 +5,7 @@ var direccio := Vector2(0, 0)
 var velocitat_maxima = 200
 var atacar:bool = false
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var collision_shape_atac = $HitBoxAtac/CollisionShape2D
 
 
 func _physics_process(delta):
@@ -12,11 +13,7 @@ func _physics_process(delta):
 	direccio = Vector2(0,0)
 	velocity.x = 0
 	velocity.y = 0
-	if (velocity.x > 1 or velocity.x < -1) or (velocity.y > 1 or velocity.y < -1):
-		animated_sprite_2d.play("Running")
-	else:
-		animated_sprite_2d.animation = "Idle"
-		
+
 	if Input.is_action_pressed("Right"):
 		direccio.x += 1
 	if Input.is_action_pressed("Left"):
@@ -27,9 +24,27 @@ func _physics_process(delta):
 		direccio.y += -1
 	if Input.is_action_just_pressed("atac"):
 		animated_sprite_2d.play("atac2")
+		collision_shape_atac.set_deferred("disabled", false)
 		atacar = true
-	velocity += direccio.normalized() * velocitat_maxima 
+		
+			
+	velocity += direccio.normalized() * velocitat_maxima
 	move_and_slide()
+	if (velocity.x > 1 or velocity.x < -1) or (velocity.y > 1 or velocity.y < -1):
+		$AnimatedSprite2D.play("Running")
+	else:
+		#animated_sprite_2d.play("Idle")
+		pass
+		
 	
 	var isLeft = velocity.x < 0
 	animated_sprite_2d.flip_h = isLeft
+
+
+func _on_animated_sprite_2d_animation_finished():
+	animated_sprite_2d.play("Idle")
+	if animated_sprite_2d.animation == "atac2":
+		collision_shape_atac.set_deferred("disabled", true)
+		atacar = false
+func _atac():
+	pass
