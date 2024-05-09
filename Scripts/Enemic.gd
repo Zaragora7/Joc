282959a623db899c9af77d7ac90dd495546299b3
rogergualbatet <rogeretgualbatet2@gmail.com>
2024-05-09 09:03:51 +0200

    aa

--- Scripts/Enemic.gd
+++ Scripts/Enemic.gd
@@ -1,31 +1,16 @@
 extends CharacterBody2D
-
-var vidas = 3
-const SPEED = 100
-var playerr = false
-var plbody = null
-var vector_knockback = Vector2()
-var força = 100
-var direccio_enemic = Vector2()
-
+var velocitat := 20
+var pl = null
+var detecta := false
 
 
 func _physics_process(delta):
-	if playerr:
-		#position += (plbody.position - position)/SPEED
-		position += (plbody.position - position)
+#ifdetecta == true:
+	#position
+	pass
 
 
-func _on_deteccio_body_entered(body):
-		plbody = body
-		playerr = true
 
-
-func _on_deteccio_body_exited(body):
-	plbody = null
-	playerr = false
-
-
-func _on_knockback_body_entered(body):
-		if playerr:
-			body.mal(global_position)
+func _on_area_detect_body_entered(body):
+	
+	queue_free()
