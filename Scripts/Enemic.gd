bce59b21575a23f1d781d90217c0bf4f0f56c57e
Zaragora7 <165767758+Zaragora7@users.noreply.github.com>
2024-05-11 12:33:05 +0200

    s'ha modificat ´¡enemic

--- Scripts/Enemic.gd
+++ Scripts/Enemic.gd
@@ -2,15 +2,33 @@ extends CharacterBody2D
 var velocitat := 20
 var pl = null
 var detecta := false
+var vidas = 3
+const SPEED = 100
+var playerr = false
+var plbody = null
+var vector_knockback = Vector2()
+var força = 100
+var direccio_enemic = Vector2()
 
 
 func _physics_process(delta):
-#ifdetecta == true:
-	#position
-	pass
+	if detecta:
+		position += (pl.position -position) / SPEED
 
 
 
-func _on_area_detect_body_entered(body):
-	
-	queue_free()
+
+
+
+func _on_deteccio_body_entered(body):
+	detecta = true
+	pl = body
+
+
+func _on_deteccio_body_exited(body):
+	detecta = false
+	pl = null
+
+
+func _on_knockback_body_entered(body):
+	pass # Replace with function body.
